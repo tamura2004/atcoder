e7f4951797a57908c254f4be14a147c17ba2b96c
tamura <tamura2004@gmail.com>
2019-10-29 23:34:01 +0900

    Add float bsearch

--- src/main.rb
+++ src/main.rb
@@ -2,13 +2,12 @@ N = gets.to_i
 L = gets.split.map(&:to_i)
 K = gets.to_i
 
-MAX = L.max
-DIG = 10**15
-
-def query(n)
-    L.map{|l| l * DIG / n }.inject(:+) - K
+def bsearch(lv, rv)
+    100.times do
+        mv = (lv + rv)/2
+        yield(mv) ? rv = mv : lv = mv
+    end
+    rv
 end
 
-ANS = (1..(MAX*DIG)).bsearch{|n| query(n)}
-printf("%d\n", ANS)
-printf("%d.%08d\n", ANS / DIG, ANS % DIG)
+puts bsearch(1.0, 100.0){|v| v * 7 > 100}
