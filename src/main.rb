81e30bb11b8571aaf45138cace79fff129cb8638
M.Tamura <tamura2004@gmail.com>
2019-10-30 18:01:09 +0900

    Add some probrem

--- src/main.rb
+++ src/main.rb
@@ -1,14 +1,5 @@
-N = gets.to_i
-L = gets.split.map(&:to_i)
-K = gets.to_i
+H,W,A,B = gets.split.map(&:to_i)
 
-MAX = L.max
-DIG = 10**15
-
-def query(n)
-    L.map{|l| l * DIG / n }.inject(:+) - K
-end
-
-ANS = (1..(MAX*DIG)).bsearch{|n| query(n)}
-printf("%d\n", ANS)
-printf("%d.%08d\n", ANS / DIG, ANS % DIG)
+S = A * H
+case S / W
+when 0..B
