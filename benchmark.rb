b1412597f4fe843a94afc523b2c2bf6f86eeab89
M.Tamura <tamura2004@gmail.com>
2019-11-01 17:05:44 +0900

    Solve abc143

--- benchmark.rb
+++ benchmark.rb
@@ -1,17 +1,47 @@
 require "benchmark/ips"
-N = 1000000
-Benchmark.ips do |x|
-    x.report("bsearch") {
-        m = (1..N).bsearch do |n|
-            n > N/2
-        end
-    }
 
-    x.report("liner") {
-        m = (1..N).find do |n|
-            n > N/2
+N = 2000
+L = Array.new(N){rand(1000)+1}
+
+def a
+    count = 0
+    for i in 0...(N-2)
+        for j in (i+1)...(N-1)
+            lb = j
+            rb = N
+            k = L[i] + L[j]
+            while rb - lb > 1
+                mb = (lb+rb)/2
+                L[mb] < k ? lb = mb : rb = mb
+            end
+            count += lb - j
         end
+    end
+    count
+end
+
+def b
+    count = 0
+    for i in 0...(N-2)
+        for j in (i+1)...(N-1)
+            lb = j
+            rb = N
+            while rb - lb > 1
+                mb = (lb+rb)/2
+                L[mb] < L[i] + L[j] ? lb = mb : rb = mb
+            end
+            count += lb - j
+        end
+    end
+    count
+end
+
+Benchmark.ips do |x|
+    x.report("a") {
+        a
+    }
+    x.report("b") {
+        b
     }
     x.compare!
 end
-
