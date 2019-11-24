05a5391d657a7f21def59ca2615f601e6ba2d5db
tamura <tamura2004@gmail.com>
2019-11-24 19:31:46 +0900

    Add loop count

--- src/main.rb
+++ src/main.rb
@@ -1,25 +1,22 @@
-N,M = gets.split.map &:to_i
-G = Array.new(N){[]}
-M.times do
-  a,b = gets.split.map &:to_i
-  G[a-1] << b-1
-  G[b-1] << a-1
-end
+N,*G = $<.map &:to_i
+M = Array.new(6,0) # visited
 
-P = [0] # パリティ
-S = [0] # スタック
+N.times do |i|
+  next unless M[i].zero? # is visited
 
-while S.size > 0
-  i = S.pop 
-  G[i].each do |j|
-    if P[j].nil?
-      P[j] = 1 - P[i]
-      S << j
-    elsif P[i] == P[j]
-      puts N * (N - 1) / 2 - M
-      exit
-    end
+  M[i] = 1
+  j = G[i]
+  l = 1
+  while i != j
+    M[j] = 1
+    j = G[j]
+    l += 1
+  end
+
+  if l.odd?
+    puts -1
+    exit
   end
 end
 
-puts P.count(0) * P.count(1) - M
+puts N/2
