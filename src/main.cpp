61ca9641e91663abe24539075ea591387e136360
M.Tamura <tamura2004@gmail.com>
2019-12-03 18:35:32 +0900

    Add dp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,40 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep(i, s, n) for (int i = s; i < n; i++)
 using namespace std;
 typedef long long ll;
-bool DP[30001][4][1000];
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+const int L = 3000;
+int dp[L+1][L+1];
 
 int main() {
-  int N; cin >> N; 
-  string S; cin >> S;
-  DP[0][0][0] = true;
-  rep(i,N) rep(j,4) rep(k,1000) {
-    if (DP[i][j][k] == false) continue;
-    DP[i+1][j][k] = true;
-    if (j < 3) {
-      DP[i+1][j+1][k*10+(S[i]-'0')] = true;
+  string s,t;
+  cin >> s >> t;
+  int n = s.size() + 1; int m = t.size() + 1;
+
+  rep(i,1,n) rep(j,1,m) {
+    if (s[i-1] == t[j-1]) {
+      dp[i][j] = dp[i-1][j-1] + 1;
+    } else {
+      chmax(dp[i][j], dp[i-1][j]);
+      chmax(dp[i][j], dp[i][j-1]);
     }
   }
-  int cnt = 0;
-  rep(i,1000) {
-    if (DP[N][3][i]) cnt++;
+
+  int idx = n;
+  while (dp[idx][m] != 0) {
+    if (dp[idx][m] == dp[idx-1][m]) {
+      idx--;
+    } else {
+      cout << s[dp[idx][m]] << ",";
+    }
   }
-  cout << cnt << endl;
+  cout << endl;
+
+  // rep(i,0,n) {
+  //   rep(j,0,m) cout << dp[i][j] << ",";
+  //   cout << "\n";
+  // }
   return 0;
 }
