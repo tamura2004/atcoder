479bcc765a10d0a7c7f758f7a9f43d421be8209b
tamura <tamura2004@gmail.com>
2020-01-18 12:03:17 +0900

    Add regex sample

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,13 @@
-require "prime"
-x = gets.to_i
-p Prime.find {|p| p >= x }
+s = gets.chomp
+if s =~ /^.*keyence$/ ||
+    s =~ /^k.*eyence$/ ||
+    s =~ /^ke.*yence$/ ||
+    s =~ /^key.*ence$/ ||
+    s =~ /^keye.*nce$/ ||
+    s =~ /^keyen.*ce$/ ||
+    s =~ /^keyenc.*e$/ ||
+    s =~ /^keyence.*$/
+    puts "YES"
+else
+    puts "NO"
+end
