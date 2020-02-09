4fd7c98266549753c3bf5a8f6dd190093a70f602
M.Tamura <tamura2004@gmail.com>
2020-02-10 06:44:50 +0900

    Add ruby code

--- src/main.rb
+++ src/main.rb
@@ -1,17 +1,21 @@
 def dist(a,b,c)
-    return ((c-a)*((b-a).conj)/((b-a).abs)).imag.abs
+    c -= a; b -= a
+    b = b.conj/b.abs
+    (c*b).imag.abs
 end
 
-o = Complex.rect(*gets.split.map(&:to_i))
-n = gets.to_i
-c = n.times.map do |i|
+def getc
     Complex.rect(*gets.split.map(&:to_i))
 end
+
+o = getc
+n = gets.to_i
+c = n.times.map { getc }
 
 ans = 1000
 n.times do |i|
     j = (i + 1) % n
     d = dist(c[i],c[j],o)
-    ans = d if ans > d
+    ans = [ans, d].min
 end
 printf("%.12f\n", ans)
