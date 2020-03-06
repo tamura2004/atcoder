d5c32ae44b82c16b40db09bdd3f9cf1a8f73ccee
M.Tamura <tamura2004@gmail.com>
2020-03-06 12:55:23 +0900

    fix: Fix segment tree bug

--- src/main.rb
+++ src/main.rb
@@ -1,23 +1,25 @@
 class SegTree
-  attr_reader :size
+  attr_reader :size, :e, :f
   attr_accessor :seg
 
-  def self.create(a=[])
+  def self.create(a=[],e,f)
     n = 1
     n *= 2 while n < a.size
-    this = new(n)
+    this = new(n,e,f)
     a.size.times do |i|
-      this.seg[i + n] = a[i]
+      this.seg[i+n] = a[i]
     end
     (n-1).downto(1) do |i|
-      this.seg[i] = this.seg[i*2] & this.seg[i*2+1]
+      this.seg[i] = f[ this.seg[i*2],this.seg[i*2+1] ]
     end
     this
   end
 
-  def initialize(n=2**32)
+  def initialize(n=2**32,e,f)
     @size = n
-    @seg = Array.new(size*2, 0)
+    @e = e
+    @f = f
+    @seg = Array.new(size*2, e)
   end
 
   def [](i)
@@ -30,16 +32,16 @@ class SegTree
     seg[i] = x
     while i > 1
       i /= 2
-      seg[i] = seg[i*2] & seg[i*2+1]
+      seg[i] = f[ seg[i*2], seg[i*2+1] ]
     end
   end
 
   def query(a,b)
-    ans = 0xffff_ffff
+    ans = e
     a += size; b += size
     while a < b
-      (ans &= seg[a]; a += 1) if a.odd?
-      (b -= 1; ans &= seg[b]) if b.odd?
+      (ans = f[ ans, seg[a] ]; a += 1) if a.odd?
+      (b -= 1; ans = f[ ans, seg[b] ]) if b.odd?
       a /= 2; b /= 2
     end
     ans
@@ -50,25 +52,40 @@ class SegTree
   end
 end
 
-# SegTree.new(N)
-# := 要素数Nで初期化、ただしNは2の冪
-# SegTree.create(A)
-# := 配列Aで初期化
-# update(i,x) := Aiをxに更新
-# query(a,b) := 半開区間[a,b)での演算結果
+# SegTree.new(N,E,F)
+# := 要素数N、単位元E、演算Fで初期化、ただしNは2の冪
+# SegTree.create(A,E,F)
+# := 配列A、単位元E、演算Fで初期化で初期化
+# update(i,x) := A[i]をxに更新
+# query(a,b) := 半開区間[a,b)でのA[i]の演算結果
 
-S = gets.chomp.chars.map{|c| 1 << c.ord - ?a.ord}
-N = S.size
-T = SegTree.create(S)
+# S = gets.chomp.chars.map{|c| 1 << c.ord - ?a.ord}
+# N = S.size
+# T = SegTree.create(S, 0xFFFF_FFFF, ->x,y{x&y})
 
-N.times do |i|
-  p T
-  if T.query(0,N-i) != 0
-    puts i
-    exit
-  end
+# N.times do |i|
+#   if T.query(0,N-i) != 0
+#     puts i
+#     exit
+#   end
 
-  (N-i-1).times do |j|
-    T.update(j, T[j] | S[j+i+1])
-  end
+#   (N-i-1).times do |j|
+#     T.update(j, T[j] | S[j+i+1])
+#   end
+# end
+
+n = gets.to_i
+a = gets.split.map &:to_i
+E = -1
+F = -> x,y { x == E ? y : y == E ? x : x.gcd(y) }
+T = SegTree.create(a,E,F)
+ans = 0
+
+n.times do |i|
+  s = T.query(0,i)
+  t = T.query(i+1,n)
+  u = F[s,t]
+  ans = u if ans < u
 end
+
+puts ans
