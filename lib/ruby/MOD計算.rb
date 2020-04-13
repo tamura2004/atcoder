bc3634a80d16e474e49042f11a32dc52350e76c0
M.Tamura <tamura2004@gmail.com>
2020-04-13 17:14:58 +0900

    feat: Add Modint to ruby

--- lib/ruby/MOD計算.rb
+++ lib/ruby/MOD計算.rb
@@ -3,27 +3,27 @@ MOD = 10 ** 9 + 7
 # MODを法とした階乗、組み合わせ
 class ModCombination
   attr_reader :fac, :inv, :finv, :mod
- 
+
   def initialize(n, mod: MOD)
     @mod = mod
     @fac = [1, 1]
     @inv = [1, 1]
     @finv = [1, 1]
- 
+
     (2..n).each do |i|
       @fac[i] = fac[i - 1] * i % mod
       @inv[i] = mod - inv[mod % i] * (mod / i) % mod
       @finv[i] = finv[i - 1] * inv[i] % mod
     end
   end
- 
+
   def combination(n, k)
     return 0 if n < k
     return 0 if n < 0 || k < 0
- 
+
     fac[n] * (finv[k] * finv[n - k] % mod) % mod
   end
-  
+
   def repeated_combination(n, k)
     combination(n + k - 1, k)
   end
@@ -57,3 +57,24 @@ x = modPow(2,n)
 y = MOD - nCk(n,a)
 z = MOD - nCk(n,b)
 puts (x + y + z - 1) % MOD
+
+class Modint < Numeric
+  MOD = 10**9+7
+  def initialize(a); @a = a.to_i % MOD; end
+  def + (other); @a = (@a + other) % MOD; self; end
+  def - (other); @a = (@a - other) % MOD; self; end
+  def * (other); @a = (@a + other) % MOD; self; end
+  def ** (other); @a = @a.pow(other, MOD); self; end
+  def / (other); @a = (@a * other.pow(MOD-2, MOD)) % MOD; end
+  def to_s; @a.to_s; end
+  def inspect; @a.to_s; end
+end
+
+class Integer
+  def to_m; Modint.new(self); end
+end
+
+10.times do |i|
+  a = (100000*i).to_m ** (i*100000)
+  puts a
+end
