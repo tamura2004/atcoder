fa95189832d74fbb3aaf4b9d6464d58b5f47cad9
M.Tamura <tamura2004@gmail.com>
2020-05-14 22:24:58 +0900

    Add nim lib

--- src/gen.rb
+++ src/gen.rb
@@ -1,6 +1,9 @@
-N = 200000
-K = 10**18
+N = 1000
+M = 1000
 open("src/input.txt", "w") do |f|
-    f.puts [N,K].join(" ")
-    f.puts Array.new(N){ rand(N) + 1 }.join(" ")
+  f.puts [N,M].join(" ")
+  M.times do
+    f.puts "."*N
+    # f.puts Array.new(2){ rand(N) + 1 }.join(" ")
+  end
 end
--- src/main.nim
+++ src/main.nim
@@ -1,36 +1,20 @@
-import sequtils,strutils,math,algorithm,tables,sets
+import sequtils,strutils,math,algorithm,tables
+when true:
+  let get = iterator: string {.closure.} = (for s in stdin.readAll.split: yield s)
+  let read = proc: int = get().parseInt
+  template rep(a,b): seq = newSeqWith(b,a)
+  template `min=`(a,b) = (if a > b: a = b)
+  template `max=`(a,b) = (if a < b: a = b)
+  const inf = 1 shl 30
+  template say(ans) = echo ["No","Yes"][1-int(ans)]
 
-let get = iterator: string {.closure.} =
-  for s in stdin.readAll.split: yield s
-proc read: int = get().parseInt
-template rep(p: untyped, n: int): seq = newSeqWith(n,p)
-template `%=`(a, b: untyped) = a = a mod b
-template chmin(a, b: untyped) =
-  if a > b: a = b
+template main =
+  let
+    n,m = read()
+    a = read().rep(n)
+    b = a.sum
+    ans = a.filterIt(it * 4 * m >= b).len >= m
 
-const MOD = 1_000_000_007i64
-let s = get().mapIt(parseInt($it))
-let n = s.len
-let d = read()
-var dp = 0i64.rep(d).rep(2).rep(n+1)
-dp[0][0][0] = 1
-
-for i in 0..<n:
-  for less in 0..1:
-    for j in 0..<d:
-      for k in 0..9:
-        if less == 0 and k > s[i]: continue
-
-        var nextLess = less
-        if k < s[i]: nextLess = 1
-        
-        let nextJ = (j + k) mod d
-        dp[i+1][nextLess][nextJ] += dp[i][less][j]
-        dp[i+1][nextLess][nextJ] %= MOD
-
-var ans = dp[n][0][0]
-ans += dp[n][1][0]
-ans %= MOD
-ans += MOD - 1i64
-ans %= MOD
-echo ans
+  say(ans)
+  
+main()
