108adf2fbfaececdf2ab241efcacc6c3129ab582
tamura <tamura2004@gmail.com>
2020-05-17 17:43:25 +0900

    fire

--- src/main.nim
+++ src/main.nim
@@ -1,15 +1,54 @@
 import sequtils,strutils,math,algorithm,tables
 when true:
-  let get = iterator: string {.closure.} = (for s in stdin.readAll.split: yield s)
-  let read = proc: int = get().parseInt
-  template rep(a,b): seq = newSeqWith(b,a)
+  let get = iterator:string {.closure.} = (for s in stdin.readAll.split: yield s)
+  let read = proc:int = get().parseInt
+  template rep(a,b):seq = newSeqWith(b,a)
   template `min=`(a,b) = (if a > b: a = b)
   template `max=`(a,b) = (if a < b: a = b)
-  type P = (int,int)
   const inf = int.high div 2
 
-proc main:int =
-  let a = read().rep(3).sorted
-  return a[^1] - a[0]
+type work = object
+  id,head,tail:int
 
-echo main()
+proc newWorks(s:string,c:int):seq[work] =
+  for i,v in s:
+    if v == 'o':
+      result.add work(id:i+1, head:i+1, tail:i+1+c)
+
+proc cmpWork(a,b:work):int =
+  case cmp(a.tail, b.tail)
+  of 1: 1
+  of -1: -1
+  else: cmp(a.head, b.head)
+
+proc takeWorks(a:var seq[work],n:int):seq[work] =
+  a.sort(cmpWork)
+  var pre = work(tail: -inf)
+  for now in a:
+    if pre.tail < now.head:
+      result.add now
+      pre = now
+    if result.len >= n: break
+  result.sort(proc(a,b:work):int = cmp(a.id,b.id))
+
+proc revWork(w:work):work =
+  result.id = w.id
+  result.head = inf - w.tail
+  result.tail = inf - w.head
+
+let n,m,c = read()
+let s = get()
+
+var a = newWorks(s,c)
+var left = takeWorks(a,m)
+
+var b = a.map(revWork)
+var right = takeWorks(b,m)
+
+var ans = false
+for i, a in left:
+  let b = right[i]
+  if a.id == b.id:
+    echo a.head
+
+
--- src/main.rb
+++ src/main.rb
@@ -1,59 +1,33 @@
-class Work
-  attr_accessor :id,:forward
-  def initialize(id,from,to,forward=true)
-    @id = id
-    @from = from
-    @to = to
-    @forward = forward
-  end
-  
-  def from; forward ? @from : @to; end
-  def to; forward ? @to : @from; end
+require "prime"
+
+# n - 1 の約数で、１以外であれば題意を満たす
+def solve_1(n)
+  Prime.prime_division(n-1).map(&:last).map(&:succ).inject(:*)-1
 end
 
-n,k,c = gets.split.map(&:to_i)
-s = gets.chomp.chars
-works = []
-s.chars.each_with_index do |ox,i|
-  next if ox == "x"
-  works << Work.new(i+1, i, i+c )
-
-
-
-a = []
-s.each_with_index do |s,i|
-  if s == "o"
-    a << [i,i+c]
+# nの約数について、割りきれなくなるまで破った後、mod==1なら題意を満たす
+# また、n,n-1の両方に含まれる約数は無い
+def solve_2(n)
+  a = []
+  1.upto(n) do |i|
+    break if i*i > n
+    if n % i == 0
+      a << i
+      a << n/i if i != n/i
+    end
   end
+  a = a - [1]
+  ans = 0
+  a.each do |i|
+    m = n
+    m /= i while m % i == 0
+    if m % i == 1
+      ans += 1
+    end
+  end
+  ans
 end
 
-a.sort_by!{|a|a[1]}
-
-F = []
-pre = -10**12
-a.each_with_index do |(x,y),i|
-  if pre < x
-    F << i
-    pre = y
-  end
-end
-
-B = []
-pre = 10**12
-a.sort!
-(a.size-1).downto(0) do |i|
-  x,y = a[i]
-  if pre > y
-    B << i
-    pre = x
-  end
-end
-F.sort!
-B.sort!
-F.zip(B).each do |x,y|
-  if x == y
-    puts a[x][0] + 1
-  end
-end
-
-    
+n = gets.to_i
+ans = solve_1(n) + solve_2(n)
+puts ans
