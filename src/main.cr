e74d2d325bac32deb16bc8db6e1455a68ba34285
M.Tamura <tamura2004@gmail.com>
2020-12-08 18:06:21 +0900

    Add avl tree

--- src/main.cr
+++ src/main.cr
@@ -1,17 +1,2 @@
-def tapecut(len,n)
-  ans = (len - 1 + n - 2) // (n - 1)
-  # pp! [len,n,ans]
-  ans.zero? ? 1 : ans
-end
-
-n,k = gets.to_s.split.map { |v| v.to_i64 }
-a = gets.to_s.split.map { |v| v.to_i64 }
-
-i = a.index(a.min).not_nil!
-case i
-when 0,n-1
-  ans = tapecut(n,k)
-else
-  ans = tapecut(i+1,k) + tapecut(n-i,k)
-end
-pp ans
+pp! ~0
+pp! ~-1
--- src/play.cr
+++ src/play.cr
@@ -1,6 +1,73 @@
-require "static_array"
+struct Nil
+  def dep
+    0
+  end
 
-a = StaticArray(Int32,2)
-a[0] = 10
-a[1] = 20
-pp! a
+  def ch
+    {nil, nil}
+  end
+
+  def print(i)
+  end
+
+  def insert(k, v)
+  end
+end
+
+class AVLTree
+  class Node
+    getter key : Int32
+    getter val : Int32
+    getter dep : Int32
+    property ch : Array(Node?)
+
+    def initialize(@key, @val)
+      @ch = Array(Node?).new(2, nil)
+      @dep = 0
+    end
+
+    def print(i = 0)
+      ch.each(&.print(i + 2))
+      puts " " * i + key.to_s
+    end
+
+    def insert(k, v)
+      b = k < key ? 0 : 1
+      ch[b] = ch[b].insert(k, v) || Node.new(k, v)
+      self
+    end
+
+    def []=(k, v)
+      insert(k, v)
+    end
+
+    def rotate
+      case ch[0].dep - c[1].dep
+      when +2
+      when -2
+      else
+      end
+    end
+  end
+
+  getter root : Node?
+
+  def initialize
+    @root = nil
+  end
+
+  def print
+    @root.try &.print
+  end
+
+  def []=(k, v)
+    @root = @root.insert(k, v) || Node.new(k, v)
+  end
+end
+
+t = AVLTree.new
+10.times do |i|
+  t[rand(100)] = rand(100)
+end
+
+t.print
