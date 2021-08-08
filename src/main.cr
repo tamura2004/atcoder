14ac2132b798f3dcdf55ddf79f14c4e8ec36fa8e
M.Tamura <tamura2004@gmail.com>
2021-08-08 15:20:35 +0900

    Add typical contest 005 half

--- src/main.cr
+++ src/main.cr
@@ -1,51 +1,60 @@
-require "crystal/grid"
+require "crystal/ntt_convolution"
 
-class Grid
-  def each_with_dir(y, x)
-    (0..3).each do |dir|
-      dy, dx = DIR[dir]
-      ny = y + dy
-      nx = x + dx
-      next if outside?(ny,nx)
-      next if wall?(ny,nx)
-      yield ny,nx,dir
+# ex. b := 4
+# c := [2,4,6,8]
+n, b, k = gets.to_s.split.map(&.to_i64)
+c = gets.to_s.split.map(&.to_i64)
+
+# [1,0,0,0]
+ans = [0_i64] * b
+ans[0] = 1_i64
+
+# [2,0,2,0]
+unit = [0_i64] * b
+c.each { |c| unit[c % b] += 1 }
+
+# 10
+d = 1_i64
+
+n.to_s(2).chars.map(&.to_i).each do |i|
+
+  # double
+  tmp = [0_i64] * b
+  ans.each_with_index do |n,m|
+    tmp[(m*d)%b] += n
+  end
+  
+  conv = convolution(ans, tmp, 10**9+7)
+  tmp.fill(0_i64)
+  conv.each_with_index do |n,m|
+    tmp[m%b] += n
+  end
+  
+  ans = tmp
+  d **= 2
+  d %= b
+ 
+  # inc
+  if i == 1
+    tmp = [0_i64] * b
+    ans.each_with_index do |n,m|
+      tmp[(m*10_i64)%b] += n
     end
-  end
-end
-
-h, w = gets.to_s.split.map(&.to_i)
-sy, sx = gets.to_s.split.map(&.to_i.- 1)
-gy, gx = gets.to_s.split.map(&.to_i.- 1)
-
-g = Grid.new(h, w, (1..h).map { gets.to_s })
-
-seen = (1..4).map { (1..h).map { [false] * w } }
-4.times do |i|
-  seen[i][sy][sx] = true
-end
-
-q = Deque.new(
-  (0..3).map do |i|
-    { 0, i, sy, sx }
-  end
-)
-
-while q.size > 0
-  cost, dir, y, x = q.shift
-  # pp! [cost,dir,y,x]
-  if y == gy && x == gx
-    puts cost
-    exit
-  end
-  seen[dir][y][x] = true
-
-  g.each_with_dir(y,x) do |ny,nx,ndir|
-    next if seen[ndir][ny][nx]
-
-    if dir == ndir
-      q.unshift({cost, ndir, ny, nx})
+    conv = convolution(tmp, unit, 10**9+7)
+    tmp.fill(0_i64)
+    conv.each_with_index do |n,m|
+      tmp[m%b] += n
+    end
+    
+    ans = tmp
+    
+    if d == 1
+      d = 10_i64
     else
-      q << {cost + 1, ndir, ny, nx}
+      d *= 10
     end
+    d %= b
   end
 end
+
+pp ans[0]
