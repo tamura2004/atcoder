d8897806276d9735cc5fd9d5dd6d8fdc46145a88
M.Tamura <tamura2004@gmail.com>
2021-09-06 09:25:56 +0900

    Add union type sample

--- src/play.cr
+++ src/play.cr
@@ -1,4 +1,38 @@
-s = Set{1,2,3}
+class Treap(T)
 
-pp s.includes?(1)
-pp s.includes?(5)
+  getter root : RealNode(T) | NilNode
+
+  def initialize
+    @root = NilNode
+  end
+
+  class RealNode(T)
+    getter size : Int32
+    property left : RealNode(T) | NilNode
+    property right : RealNode(T) | NilNode
+
+    def initialize
+      @size = 1
+      @left = NilNode
+      @right = NilNode
+    end
+  end
+
+  module NilNode
+    extend self
+
+    def size
+      0
+    end
+
+    def left
+      NilNode
+    end
+
+    def left=(v)
+    end
+  end
+end
+
+t = Treap(Int64).new
+t.root.left = Treap::RealNode(Int64).new
