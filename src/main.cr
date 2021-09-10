1a60bd43f2290a0a41989ce976a583e2e6fb4ccc
M.Tamura <tamura2004@gmail.com>
2021-09-10 20:05:42 +0900

    Solve some problem

--- src/main.cr
+++ src/main.cr
@@ -1,31 +1,23 @@
-require "crystal/mod_int"
+require "crystal/treap_merge_split"
 
-def poww(a, n)
-  return a if n == 1
-  return poww(prod(a, a), n//2) if n % 2 == 0
-  prod(a, poww(a, n - 1))
-end
+n, m = gets.to_s.split.map(&.to_i64)
+tr = Treap{m}
 
-def prod(a, b)
-  k = a.size
-  c = [0.to_m] * (k * 2 - 1)
+ans = 0_i64
+n.times do
+  c = gets.to_s.to_i64
+  cnt = Int64::MAX
 
-  k.times do |i|
-    k.times do |j|
-      c[i + j] += a[i] * b[j]
-    end
+  if lo = tr.lower(c)
+    chmin cnt, (lo - c).abs
   end
 
-  (k...k*2 - 1).reverse_each do |i|
-    k.times do |j|
-      c[i - k + j] += c[i]
-    end
+  if hi = tr.upper(c)
+    chmin cnt, (hi - c).abs
   end
 
-  c[...k]
+  ans += cnt
+  tr << c
 end
 
-k, n = gets.to_s.split.map(&.to_i64)
-a = [0.to_m, 1.to_m] + [0.to_m] * (k - 2)
-ans = poww(a, n - 1).sum
 pp ans
