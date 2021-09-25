afb9a90f6f54cf837d21dabe2f55f5f51ef9122e
M.Tamura <tamura2004@gmail.com>
2021-09-26 08:54:20 +0900

    Add problem

--- src/main.cr
+++ src/main.cr
@@ -1,14 +1,27 @@
-require "crystal/indexable"
-require "crystal/weighted_graph/dijkstra"
-include WeightedGraph
+require "crystal/digit_dp"
 
-n,m,r,t = gets.to_s.split.map(&.to_i)
-g = Graph.new(n)
-m.times do
-  v,nv,cost = gets.to_s.split.map(&.to_i64)
-  g.add v, nv, cost, origin: 1, both: true
+n = gets.to_s
+
+dd = DigitDP.new(n)
+
+dp = make_array(0_i64, dd.n + 1, 16, 2,2)
+dp[0][0][0][0] = 1_i64
+
+dd.each do |i,k,d,kk|
+  16.times do |j|
+    2.times do |m|
+      jj = j + ((d == 1 && m == 1) ? 1 : 0)
+      next if jj >= 16
+      mm = d == 1 ? 1 : 0
+      dp[i+1][jj][kk][mm] += dp[i][j][k][m]
+    end
+  end
 end
 
-g.debug
+ans = 0_i64
+16.times do |j|
+  ans += dp[-1][j].map(&.sum).sum
+end
 
-
+pp dp[-1]
+pp ans - 1
