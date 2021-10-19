41ba52adcc30cb9e1a85015b815e79a8f82bfdeb
M.Tamura <tamura2004@gmail.com>
2021-10-19 18:23:20 +0900

    Update lib

--- src/play.cr
+++ src/play.cr
@@ -1,5 +1,41 @@
-x = [{1,2},{3,4}]
-
-x.each_with_index do |(a,b),i|
-  pp [a,b,i]
+n, k = gets.to_s.split.map(&.to_i)
+a = gets.to_s.split.map(&.to_i.- 1)
+s = Array.new(n) do
+  gets.to_s
 end
+
+cnt = Array.new(n, false)
+a.each do |i|
+  cnt[i] = true
+end
+
+t = (0...n).map do |i|
+  { s[i], cnt[i] }
+end.sort_by(&.first)
+
+lo = 0
+while lo < n && !t[lo].last
+  lo += 1
+end
+
+hi = lo
+while hi < n && t[hi].last
+  hi += 1
+end
+
+if (hi - lo) != k
+  puts -1
+  exit
+end
+
+head = t[lo].first
+tail = t[hi-1].first
+i = 0
+max = Math.min head.size, tail.size
+while i < max && head[i] == tail[i]
+  i += 1
+end
+# pp! s
+# pp! head
+# pp! tail
+puts head[0,i]
