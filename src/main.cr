cfb903b764c8b25d7266733b4d1d639b35c25cab
M.Tamura <tamura2004@gmail.com>
2021-11-04 19:05:21 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,42 +1,20 @@
 n = gets.to_s.to_i
-ans = Array.new(n) { Array.new(n, '.') }
+s = gets.to_s.chars.map(&.ord.- 'A'.ord)
+t = gets.to_s.chars.map(&.ord.- 'A'.ord)
+puts conv(s) == conv(t) ? "Yes" : "No"
 
-x1 = y1 = 0
-x2 = y2 = n - 2
+def conv(a)
+  q = Deque.new([] of Int32)
 
-while n > 0
-  box(y1,y2,x1,x2,ans)
-  x1 += 2
-  y2 -= 4
-  x2 -= 2
-  n -= 4
-end
+  a.each do |v|
+    q << v
+    next if q.size < 3
 
-puts ans.map(&.join).join("\n")
-
-def box(y1, y2, x1, x2, ans)
-  vline(y1, y2, x1, ans)
-  hline(y2, x1, x2, ans)
-  vline(y1, y2 - 2, x2, ans)
-  hline(y2 - 2, x1 + 2, x2, ans)
-end
-
-def vline(y1, y2, x, ans)
-  return unless 0 <= y1
-  return unless 0 <= y2
-  return unless 0 <= x
-  return unless y1 <= y2
-  (y1..y2).each do |y|
-    ans[y][x] = '#'
+    case {q[-3], q[-2], q[-1]}
+    when {0, 1, 2}, {1, 2, 0}, {2, 0, 1}
+      q.pop(3)
+    end
   end
-end
 
-def hline(y, x1, x2, ans)
-  return unless 0 <= y
-  return unless 0 <= x1
-  return unless 0 <= x2
-  return unless x1 <= x2
-  (x1..x2).each do |x|
-    ans[y][x] = '#'
-  end
+  q
 end
