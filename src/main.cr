bc04d113e134726744c6bb93ae59d86d25863467
M.Tamura <tamura2004@gmail.com>
2021-11-08 20:08:03 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,15 +1,10 @@
-def pf(n, k, a, &block : Array(Int32) -> Nil)
+require "crystal/modint9"
+
+def pf(n, k, a, &block : Array(Int64) -> Nil)
   case
-  when n == 0
-    yield a
-  when n == 1
-    a << 1
-    yield a
-    a.pop
-  when k == 1
-    n.times { a << 1 }
-    yield a
-    a.pop(n)
+  when n == 0 then yield a
+  when n == 1 then yield a
+  when k == 1 then yield a
   else
     if k <= n
       a << k
@@ -21,9 +16,23 @@ def pf(n, k, a, &block : Array(Int32) -> Nil)
   end
 end
 
-ans = 0_i64
-pf(10, 10, [] of Int32) do |a|
-  pp a
+def calc(a, n, k, nf)
+  return 1_i64 if a.empty?
+
+  x1 = a.reduce { |s, t| s.lcm(t) }
+  x2 = a.map(&.pred).product
+  x3 = a.map(&.f).reduce { |s, t| s * t }
+  x4 = (n - a.sum).f
+  x1.to_m ** k * x2 * nf // x3 // x4
+end
+
+n, k = gets.to_s.split.map(&.to_i64)
+
+ans = 0.to_m
+nf = n.f
+
+pf(n, n, [] of Int64) do |a|
+  ans += calc(a, n, k, nf)
 end
 
 pp ans
