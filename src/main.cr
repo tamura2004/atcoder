98db86a146f260366759b2e764f0c86591e14aff
M.Tamura <tamura2004@gmail.com>
2021-11-17 22:21:11 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,18 +1,24 @@
-require "crystal/segment_tree"
-
 n = gets.to_s.to_i
 a = gets.to_s.split.map(&.to_i)
 
-def solve(a)
-  st = SegmentTree(Int64).range_max_query(a.max+1)
-  a.each do |i|
-    st[i] = st[i+1..] + 1
+cnt = a.zip(0..).group_by(&.first).transform_values(&.map &.last)
+
+ans = 1
+i = 0
+cnt.keys.sort.each do |key|
+  if ix = cnt[key].bsearch_index(&.>= i)
+    if cnt[key].size == 1
+      i = cnt[key][ix]
+    elsif ix == 0
+      i = cnt[key][-1]
+    else
+      ans += 1
+      i = cnt[key][ix-1]
+    end
+  else
+    ans += 1
+    i = cnt[key][-1]
   end
-  st[0..]
 end
 
-cnt = solve(a)
-a.rotate!
-ans = Math.min cnt, solve(a)
-pp ans
-
+puts i == 0 ? ans - 1 : ans
