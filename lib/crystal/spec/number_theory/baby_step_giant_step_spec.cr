45829ad2de7101cb3d70808396e12787aa43de0c
M.Tamura <tamura2004@gmail.com>
2021-11-24 17:25:57 +0900

    Update lib

---
+++ lib/crystal/spec/number_theory/baby_step_giant_step_spec.cr
@@ -0,0 +1,14 @@
+require "spec"
+require "crystal/number_theory/baby_step_giant_step"
+include NumberTheory
+
+describe NumberTheory::BabyStepGiantStep do
+  it "usage" do
+    x = 3_i64
+    y = 193_i64
+    mod = 10_i64**9 + 7
+
+    r = BabyStepGiantStep.solve(x, y, mod)
+    BabyStepGiantStep.modpow(x, r, mod).should eq y
+  end
+end
