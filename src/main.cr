896e05e56f16fdba55f57842b8fc653ccdda1905
M.Tamura <tamura2004@gmail.com>
2022-01-26 18:59:29 +0900

    Update dfa

--- src/main.cr
+++ src/main.cr
@@ -1,19 +1,37 @@
-require "crystal/mod_int"
 require "crystal/dfa"
 
-m = gets.to_s.to_i
+record ModInt, v : Int64 do
+  MOD = 10 ** 9 + 7
+  def +(b); ModInt.new (v + b.to_i64) % MOD; end
+  def -(b); ModInt.new (v - b.to_i64) % MOD; end
+  def self.zero; new(0); end
+  def to_i64; v; end
+  delegate to_s, inspect, to: v
+end
+
+class DigitSumMultipleOf(T) < DFA(Int32,Int32,T)
+  getter m : Int32
+
+  def initialize(@m)
+    @init = 0
+    @nex = ->(s : Int32, d : Int32, i : Int32) {
+      (s + d) % m
+    }
+    @accept = ->(s : Int32) { s.zero? }
+    @ok = ->(s : Int32) { false }
+    @ng = ->(s : Int32) { false }
+  end
+end
+
+# main
 a = gets.to_s.chars.map(&.to_i)
+m = gets.to_s.to_i
 n = a.size
 
+dsmo = DigitSumMultipleOf(ModInt).new(m)
 leq = LEQ(ModInt).new(a)
 
-mod = DFA(Int32, Int32, ModInt).new(
-  init: 0,
-  nex: ->(s : Int32, d : Int32, i : Int32) { (s + d) % m },
-  accept: ->(s : Int32) { s == 0 },
-  ok: ->(s : Int32) { false },
-  ng: ->(s : Int32) { false },
-)
+D = (0..9).to_a
+ans = (dsmo + leq).count(n, D)
 
-ans = (leq + mod).count(n, (0..9).to_a)
 pp ans - 1
--- src/play.cr
+++ src/play.cr
@@ -1,17 +1,8 @@
-record ModInt, v : Int64 do
-  MOD = 998_244_353_i64
-  def +(b); ModInt.new((v + b.to_i64 % MOD) % MOD); end
-  def -(b); ModInt.new((v + MOD - b.to_i64 % MOD) % MOD); end
-  def *(b); ModInt.new((v * (b.to_i64 % MOD)) % MOD); end
-  def self.zero; new(0); end
-  delegate to_i64, to: v
-  delegate to_s, to: v
-  delegate inspect, to: v
-end
+record Key, v : Int32
 
-struct Int
-  def to_m
-    ModInt.new(to_i64)
-  end
-end
+cnt = Hash(Key,Int32).new
+key1 = Key.new(10)
+key2 = Key.new(10)
 
+pp key1.hash
+pp key2.hash
