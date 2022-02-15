3a834205e52780ca1f3c82d6a1451fc614668695
M.Tamura <tamura2004@gmail.com>
2022-02-15 20:09:22 +0900

    Update lib

--- old/abc010a_addition.rb
+++
@@ -1,5 +0,0 @@
-N = gets.to_i
-A = gets.split.map(&:to_i)
-S = A.inject(:+)
-
-puts S.even? ? "YES" : "NO"
--- old/tally.cr
+++
@@ -1,7 +0,0 @@
-module Enumerable(T)
-  def tally
-    each_with_object(Hash(T,Int64).new(0_i64)) do |v,h|
-      h[v] += 1
-    end
-  end
-end
