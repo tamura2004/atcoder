0ec9d67167034dc7749eedd7ff240de846d7a829
M.Tamura <tamura2004@gmail.com>
2022-03-06 15:11:52 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,5 +1,34 @@
-ans = 10.times.max_of do |x|
-  10-x
+require "crystal/modint9"
+
+# 26進数として変換
+def conv(a)
+  ans = 0.to_m
+  a.chars.map(&.ord.- 'A'.ord).each do |v|
+    ans *= 26
+    ans += v
+  end
+  ans + 1
 end
 
-pp ans
+def left(a)
+  n = divceil(a.size, 2)
+  a[0,n]
+end
+
+def right(a)
+  n = divceil(a.size, 2)
+  a[-n..]
+end
+
+t = gets.to_s.to_i
+t.times do
+  n = gets.to_s.to_i
+  s = gets.to_s
+
+  ans = conv(left(s))
+  if right(s) < left(s).reverse
+    ans -= 1
+  end
+  
+  pp ans
+end
--- src/main.rb
+++ src/main.rb
@@ -1,18 +1,1 @@
-n,t,m=gets.split.map &:to_i
-a=x=1
-c=[x]
-(1..t).each{|i|
-j=t+1-i
-x*=j
-x/=i
-c<<x%m
-}
-n.times{
-i,j=gets.split.map &:to_i
-[(i+j).abs,(i-j).abs].each{|x|
-(t-x).odd?||t<x&&p(0)+exit
-a*=c[(t-x)/2]
-a%=m
-}
-}
-p a
+puts "hello"
