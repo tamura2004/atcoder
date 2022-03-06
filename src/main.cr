471ba984e87338fd219466aa0094191a6a0878bd
M.Tamura <tamura2004@gmail.com>
2022-03-06 21:20:21 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,34 +1,18 @@
-require "crystal/modint9"
+require "crystal/fenwick_tree"
 
-# 26進数として変換
-def conv(a)
-  ans = 0.to_m
-  a.chars.map(&.ord.- 'A'.ord).each do |v|
-    ans *= 26
-    ans += v
-  end
-  ans + 1
+n = gets.to_s.to_i
+a = gets.to_s.split.map(&.to_i64)
+b = gets.to_s.split.map(&.to_i64)
+
+quit("No") if a.sort != b.sort
+
+def norm(a, b)
+  ai = a.zip(0..).sort.map(&.last)
+  bi = b.zip(0..).sort.map(&.last)
+  ai.zip(bi).sort.map(&.last.+ 1)
 end
 
-def left(a)
-  n = divceil(a.size, 2)
-  a[0,n]
-end
+cnt = inversion_number(norm(a, b))
+puts cnt.odd? ? "No" : "Yes"
 
-def right(a)
-  n = divceil(a.size, 2)
-  a[-n..]
-end
-
-t = gets.to_s.to_i
-t.times do
-  n = gets.to_s.to_i
-  s = gets.to_s
-
-  ans = conv(left(s))
-  if right(s) < left(s).reverse
-    ans -= 1
-  end
-  
-  pp ans
-end
+pp [3,1,1,5,1,1,2].uniq
