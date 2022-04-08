a67fb1e32151be2676292336219b121daf256993
M.Tamura <tamura2004@gmail.com>
2022-04-08 21:10:59 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,54 +1,28 @@
-require "crystal/mod_int"
-require "crystal/fact_table"
+require "crystal/indexable"
+require "crystal/avl_tree"
 
-n, m, l = gets.to_s.split.map(&.to_i64)
+n,m = gets.to_s.split.map(&.to_i64)
+a = gets.to_s.split.map(&.to_i64).sort
+b = gets.to_s.split.map(&.to_i64).sort
+teacher = b.to_ordered_set
 
-# dp[i頂点利用][j辺利用] := 種類数
-# 初期化：長さLのパスを含む、長さLのループを含む
-# 遷移：
-# 残りの頂点を１～利用
-# ループか、パスか
+c = (0...n).map{|i|i.odd? ? a[i] : -a[i]}
+d = c.map(&.-)
 
-quit(0) if m < l - 1 # パスも作れない
+left = c.cs
+right = d.csr
 
-ans = 0.to_m
-
-dp = make_array(0.to_m, n + 1, m + 1)
-if l == 1
-  dp[l][l - 1] = n.to_m
-else
-  dp[l][l - 1] = n.c(l) * l.f // 2
-end
-
-if l <= m && l != 1
-  dp[l][l] = n.c(l) * (l - 1).f // 2
-end
-
+ans = Int64::MAX
 n.times do |i|
-  m.times do |j|
-    (1..n - i).each do |use|
-      next if l < use
-      2.times do |k|
-        ii = i + use
-        jj = j + use - k
+  cnt = left[i] + right[i+1]
 
-        next if use == 1 && k == 0
+  teacher.lower(a[i]).try do |t|
+    chmin ans, cnt + (t - a[i]).abs
+  end
 
-        next if n < ii
-        next if m < jj
-
-        if k == 1
-          if use == 1
-            dp[ii][jj] += dp[i][j] * (n - i)
-          else
-            dp[ii][jj] += dp[i][j] * (n - i).c(use) * use.f * 2.inv
-          end
-        else
-          dp[ii][jj] += dp[i][j] * (n - i).c(use) * (use - 1).f * 2.inv
-        end
-      end
-    end
+  teacher.upper(a[i]).try do |t|
+    chmin ans, cnt + (t - a[i]).abs
   end
 end
 
-pp dp[-1][-1]
+pp ans
