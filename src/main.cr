c8cdc58c9bcce81969ecaef436970d8637f7a09c
M.Tamura <tamura2004@gmail.com>
2022-04-11 21:31:00 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,16 +1,42 @@
-require "crystal/matrix"
+# 全てのｋについて、c_k開始の最短距離を
+# 幅優先検索で求める
+# c_iの重み付きグラフを作る
+# TSPを解く
+# 距離＋１が答え
 
-n = gets.to_s.to_i64
-st = Array.new(n) do
-  a,b,c = gets.to_s.split.map(&.to_i64)
-  a = a.to_i - 1
-  b = b.to_i - 1
-  {a,b,c}
+require "crystal/graph"
+
+class BFS
+  getter g : Graph
+  delegate n, to: g
+
+  def initialize(@g)
+  end
+
+  def solve(root = 0)
+    q = Deque.new([root])
+    seen = Hash(Int32,Bool).new(false)
+    seen[root] = true
+    dist = Array.new(n, 0)
+
+    while q.size > 0
+      v = q.shift
+      g[v].each do |nv|
+        next if seen[nv]
+        seen[nv] = true
+        dist[nv] = dist[v] + 1
+        q << nv
+      end
+    end
+    dist
+  end
 end
 
-m = Matrix(Int64).zero(20)
-st.each do |a,b,c|
-  chmax m[a][b], c
-end
+g = Graph.new(5)
+g.add 1, 2
+g.add 3, 2
+g.add 2, 4
+g.add 1, 5
 
-pp m
+pp BFS.new(g).solve(0)
+pp BFS.new(g).solve(4)
