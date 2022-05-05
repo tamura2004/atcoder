14ff4d4ddbb790158857af949a3be7b2f912f191
M.Tamura <tamura2004@gmail.com>
2022-05-05 20:26:00 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,41 +1,33 @@
-require "crystal/flow_graph/dinic"
-include FlowGraph
+# require "crystal/weighted_flow_graph/min_cost_flow"
+# include WeightedFlowGraph
 
-INF = Int64::MAX
+# red, green, blue = gets.to_s.split.map(&.to_i64)
+# n = 803
 
-h, w = gets.to_s.split.map(&.to_i64)
-a = Array.new(h) { gets.to_s }
+# g = Graph.new(n)
 
-g = Graph(Int32).new(h + w + 2)
-# 縦 0...h
-# 横 h...h+w
-# 始点 h+w
-# 終点 h+w+1
+# source = 801
+# sink = 802
 
-root = h + w
-goal = root + 1
+# g.add source, 300, red, 0
+# g.add source, 400, green, 0
+# g.add source, 500, blue, 0
 
-sy = sx = gy = gx = -1
+# (0..800).each do |v|
+#   g.add v, sink, 1, 0
+# end
 
-h.times do |y|
-  w.times do |x|
-    case a[y][x]
-    when 'S'
-      sy, sx = y, x
-      g.add root, y, INF
-      g.add root, h + x, INF
-    when 'T'
-      gy, gx = y, x
-      g.add y, goal, INF
-      g.add h + x, goal, INF
-    when 'o'
-      g.add y, h + x, 1
-      g.add h + x, y, 1
-    end
-  end
+# [300,400,500].each do |v|
+#   (-300..300).each do |offset|
+#     nv = v + offset
+#     cost = offset.abs
+#     g.add v, nv, 1, cost
+#   end
+# end
+
+# ans = MinCostFlow.new(g).solve(source, sink, red + green + blue)
+# pp ans
+
+(-10..10).each do |v|
+  pp v
 end
-
-quit -1 if sy == gy || sx == gx
-
-ans = Dinic(Int32).new(g).solve(root, goal)
-pp ans
