e69b491a31deb8ee4042125266a098d07bf6fe44
M.Tamura <tamura2004@gmail.com>
2022-05-27 10:50:10 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,17 +1,46 @@
-require "crystal/range_tree"
+# ABC106_D
 
-n = gets.to_s.to_i
-rt = RangeTree.new(n)
-a = gets.to_s.split.map(&.to_i64)
-
-a.each_with_index do |x, y|
-  rt.add y, x
+# イベントソート用に種別をenumで定義
+enum EventType
+  Train
+  Towns
 end
 
-q = gets.to_s.to_i
-q.times do
-  l, r, x = gets.to_s.split.map(&.to_i64)
-  l = l.to_i.pred
-  r = r.to_i.pred
-  pp rt[l..r, x..x]
+# イベントのリストを定義
+events = [] of Tuple(Int32, EventType, Int32, Int32)
+
+# 問題の入力を読み込み
+n, m, q = gets.to_s.split.map(&.to_i)
+
+# 列車の情報をイベントに追加
+m.times do
+  left, right = gets.to_s.split.map(&.to_i.pred)
+  events << {right, EventType::Train, left, -1}
 end
+
+# クエリ（左右の都市）をイベントに追加
+q.times do |index|
+  left, right = gets.to_s.split.map(&.to_i.pred)
+  events << {right, EventType::Towns, left, index}
+end
+
+# 右端の昇順、イベント種類順（列車＞都市）順にソート
+events.sort!
+
+# 始発駅の数
+stations = Array.new(500, 0_i64)
+
+# 回答
+answers = Array.new(q, -1i64)
+
+# イベントを順次処理
+events.each do |right, event, left, index|
+  case event
+  when .train? # enum定義時に小文字の問い合わせメソッドが定義される
+    stations[left] += 1
+  when .towns?
+    answers[index] = stations[left..right].sum
+  end
+end
+
+puts answers.join("\n")
