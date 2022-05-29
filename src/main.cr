ef2cc3e355ba77229f7bcdb127e00f34330f4c29
tamura <tamura2004@gmail.com>
2022-05-29 17:01:56 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,37 +1,13 @@
-# q = gets.to_s.to_i
+n = gets.to_s.to_i
+s = gets.to_s.chars
 
-# cnt = {} of Int64 => Int64
-# xs = [] of Int64
+cnt = s.chunks(&.itself).map{|a,arr|{a,arr.size.to_i64}}
+st = [] of Int64
 
-# q.times do
-#   query = gets.to_s
+cnt.each_cons(3) do |(s,t,u)|
+  if s[0] == 'A' && t[0] == 'R' && u[0] == 'C' && t[1] == 1
+    st << Math.min(s[1],u[1])
+  end
+end
 
-#   case query
-#   when /^1/
-#     _, x = query.split.map(&.to_i64)
-
-#     if cnt.has_key?(x) && cnt[x] > 0
-#       cnt[x] += 1
-#     else
-#       i = xs.bsearch_index(&.> x) || xs.size
-#       xs.insert(i, x)
-#       cnt[x] = 1_i64
-#     end
-#   when /^2/
-#     _, x, c = query.split.map(&.to_i64)
-    
-#     next if !cnt.has_key?(x)
-#     cnt[x] -= Math.min(c, cnt[x])
-    
-#     next if 0 < cnt[x]
-#     xs.bsearch_index(&.>= x).try do |i|
-#       xs.delete_at(i)
-#     end
-#   when /^3/
-#     puts xs[-1] - xs[0]
-#   end
-# end
-
-pp! 2 | 1 << 4 - 3
-pp! 1 < 2 < 3
-pp! 1 > 2 > 3
+pp Math.min(st.sum, n * 2)
