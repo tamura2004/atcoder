cfac037b98422d536af06b483adf3d2f3b3761e2
M.Tamura <tamura2004@gmail.com>
2022-05-29 21:22:05 +0900

    Update lib

--- src/main.cr
+++ src/main.cr
@@ -1,13 +1,25 @@
 n = gets.to_s.to_i
 s = gets.to_s.chars
 
-cnt = s.chunks(&.itself).map{|a,arr|{a,arr.size.to_i64}}
-st = [] of Int64
+nums = [] of Int64
+chars = [] of Char
 
-cnt.each_cons(3) do |(s,t,u)|
-  if s[0] == 'A' && t[0] == 'R' && u[0] == 'C' && t[1] == 1
-    st << Math.min(s[1],u[1])
+s.each do |c|
+  if chars.empty? || chars[-1] != c
+    chars << c
+    nums << 1_i64
+  else
+    nums[-1] += 1
   end
 end
 
-pp Math.min(st.sum, n * 2)
+cnt = [] of Int64
+
+chars.zip(nums).each_cons(3) do |seq|
+  if seq.map(&.first) == ['A','R','C'] && seq[1][1] == 1
+    cnt << Math.min seq[0][1], seq[2][1]
+  end
+end
+
+pp Math.min cnt.size * 2, cnt.sum
+
