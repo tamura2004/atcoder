564d406f6cb71696698aa5d5acbee0c963e4bbe4
M.Tamura <tamura2004@gmail.com>
2022-06-09 23:00:07 +0900

    Update lib

--- lib/crystal/spec/string_scanner_sample_spec.cr
+++ lib/crystal/spec/string_scanner_sample_spec.cr
@@ -11,5 +11,8 @@ describe StringScannerSample do
     SSS.new("1+2*(3+4)").expr.should eq 15
     SSS.new("1+(1+2*3)").expr.should eq 8
     SSS.new("1+(1+(1+(1+(1+2*3))))").expr.should eq 11
+    SSS.new("1234+5432").expr.should eq 6666
+    SSS.new("12*11").expr.should eq 132
+    SSS.new("(5+7)*(1+5*2)").expr.should eq 132
   end
 end
--- src/main.cr
+++ src/main.cr
@@ -1,47 +1,57 @@
-require "crystal/indexable"
+# さて、レベルNバーガーは何層だろうか？
+# f(N) = 層数とする
+# f(0) = 1
+# f(N+1) = f(N) * 2 + 3
+# ではパティは何枚含むだろうか
+# ps(N) = パティ数とする
+# ps(0) = 1
+# ps(N+1) = ps(N) * 2 + 1
+# N <= 50なので、予め求めておくことができる
+# ちなみに、レベルNバーガーの丁度真ん中のパティを食べるときの枚数
+# pi(0)=1
+# pi(n+1) = f(n) + 2
 
-s = gets.to_s
-x = gets.to_s.to_i64
+n, x = gets.to_s.split.map(&.to_i64)
+f = [1_i64]
+ps = [1_i64]
+pi = [1_i64]
+n.times do
+  pi << f[-1] + 2
+  f << f[-1] * 2 + 3
+  ps << ps[-1] * 2 + 1
+end
 
-puts Problem.new(s).solve(x)
+# pp! f
+# pp! ps
+# pp! pi
 
-class Problem
-  getter s : String
-  getter n : Int32
-  getter len : Array(Int64)
+# 今、レベルnバーガーのx枚目を気にしている
+# 中央のパティより右であれば、左はレベルn-1バーガーの
+# パティ数をpより取れば良い。
+# 右は、レベルn-1バーガーからのx'=x-pi(n)枚食べたとき
+# であり、小問題に分割された。
+# 丁度中央のパティを食べる場合に注意
 
-  def initialize(@s)
-    @n = s.size
-    @len = [] of Int64
-    n.times do |i|
-      case s[i]
-      when 'a'..'z'
-        if len.empty?
-          len << 1_i64
-        else
-          len << Math.min(len[-1] + 1, 1e16.to_i64)
-        end
-      when '0'..'9'
-        if len.empty?
-          len << 0_i64
-        else
-          d = s[i].to_i64
-          len << Math.min(len[-1] * (d + 1), 1e16.to_i64)
-        end
-      end
-    end
-  end
+solve = uninitialized Proc(Int64, Int64, Int64)
+solve = ->(n : Int64, x : Int64) do
+  # pp! [n,x]
+  return ps[n] if f[n] <= x
 
-  def solve(x)
-    (0...n).reverse_each do |i|
-      next if x <= len[i]
+  raise "bad negative number #{x}" if x < 0
+  raise "bad negative level #{n}" if n < 0
 
-      if s[i + 1].in?('a'..'z')
-        return s[i + 1]
-      else
-        x = (x - 1) % len[i] + 1
-      end
-    end
-    return s[0]
+  return 0_i64 if x == 0
+  raise "bad n = #{n}, x = #{x}" if n == 0 && x > 1
+  return 1_i64 if n == 0
+
+  if x < pi[n]
+    solve.call(n - 1, x - 1)
+  elsif x == pi[n]
+    ps[n - 1] + 1
+  else
+    ps[n - 1] + 1 + solve.call(n - 1, x - pi[n])
   end
 end
+
+ans = solve.call(n, x)
+pp ans
