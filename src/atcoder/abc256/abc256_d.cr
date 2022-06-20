3ce798cdb804e028c3bcf333bf80106ca0169c33
M.Tamura <tamura2004@gmail.com>
2022-06-20 21:18:05 +0900

    Add problem

--- src/atcoder/abc256/abc256_d.cr
+++ src/atcoder/abc256/abc256_d.cr
@@ -1,20 +1,50 @@
-require "crystal/range_set"
+# ライブラリ版
+# require "crystal/range_set"
 
-set = RangeSet.new
-n = gets.to_s.to_i64
+# set = RangeSet.new
+# n = gets.to_s.to_i64
 
-n.times do
-  lo, hi = gets.to_s.split.map(&.to_i64)
-  set << (lo...hi)
+# n.times do
+#   lo, hi = gets.to_s.split.map(&.to_i64)
+#   set << (lo...hi)
+# end
+
+# ans = [] of Int64
+
+# set.each do |(i, side)|
+#   ans << i
+
+#   if side.hi?
+#     puts ans.join(" ")
+#     ans.clear
+#   end
+# end
+
+# 始点でソートして貪欲
+
+n = gets.to_s.to_i
+a = Array.new(n) do
+  lo, hi = gets.to_s.split.map(&.to_i)
+  {lo,hi}
 end
+a.sort!
 
-ans = [] of Int64
+ans = [] of Tuple(Int32,Int32)
 
-set.each do |(i, side)|
-  ans << i
-
-  if side.hi?
-    puts ans.join(" ")
-    ans.clear
+a.each do |lo, hi|
+  case
+  when ans.empty?
+    ans << {lo, hi}
+  when lo <= ans[-1][1] && ans[-1][1] < hi
+    pre_lo, pre_hi = ans.pop
+    ans << {pre_lo, hi}
+  when ans[-1][1] < lo
+    ans << {lo, hi}
   end
 end
+
+ans.each do |lo,hi|
+  puts "#{lo} #{hi}"
+end
+
+
--- src/main.cr
+++ src/main.cr
@@ -1,15 +1,13 @@
-require "crystal/balanced_tree/treap/multiset"
+require "crystal/range_set"
 
-enum R
-  LO
-  HI
+n, m = gets.to_s.split.map(&.to_i)
+st = RangeSet.new
+
+m.times do
+  lo, hi = gets.to_s.split.map(&.to_i64)
+  st << (lo...hi)
 end
 
-ms = Multiset(Tuple(Int32,R)).new
-
-n = gets.to_s.to_i64
-n.times do
-  lo, hi = gets.to_s.split.map(&.to_i)
-  ms << {lo, R::LO}
-  ms << {hi, R::HI}
-end
+# ans = st.size
+# pp ans
+pp st
