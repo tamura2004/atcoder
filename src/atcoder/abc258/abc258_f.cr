07cf4a680fd3d8b3e41b8dae5fec05c4832dd2b3
tamura <tamura2004@gmail.com>
2022-07-03 13:49:22 +0900

    Add problem

--- src/atcoder/abc258/abc258_f.cr
+++ src/atcoder/abc258/abc258_f.cr
@@ -1,5 +1,12 @@
 require "crystal/complex"
 
+enum Road
+  Cross
+  Tate
+  Yoko
+  Town
+end
+
 class Problem
   getter b : Int64
   getter k : Int64
@@ -12,28 +19,69 @@ class Problem
   def solve
     ans = (sz - gz).manhattan * k
 
-    each_candidate(sz) do |z, zcost|
-      each_candidate(gz) do |w, wcost|
-        pp! [z,w,zcost,wcost]
-        chmin ans, (z - w).manhattan + zcost + wcost
+    each(sz) do |z, zc|
+      each(gz) do |w, wc|
+        pp! [z, w, zc, wc, dist(z,w)]
+        chmin ans, dist(z, w) + zc + wc
       end
     end
 
     ans
   end
 
-  def each_candidate(z)
-    [
-      {1, 1},
-      {1.i, -1.i},
-      {-1, -1},
-      {-1.i, 1.i},
-    ].each do |rot, rv|
-      w = z * rot
+  def dist(z, w)
+    cnt = (z - w).manhattan
+    case type(z)
+    when .tate?
+      if type(w).tate?
+        if (z // b).imag == (w // b).imag
+          d1 = (z.imag % b + w.imag % b) + (z.real - w.real).abs
+          d2 = ((b - z.imag % b) + (b - w.imag % b)) + (z.real - w.real).abs
+          pp! [d1,d2]
+          Math.min d1, d2
+        else
+          cnt
+        end
+      else
+        cnt
+      end
+    when .yoko?
+      if type(w).yoko?
+        if (z // b).real == (w // b).real
+          d1 = (z.real % b + w.real % b) * k + (z.imag - w.imag).abs
+          d2 = ((b - z.real % b) + (b - w.real % b)) * k + (z.imag - w.imag).abs
+          Math.min d1, d2
+        else
+          cnt
+        end
+      else
+        cnt
+      end
+    else
+      cnt
+    end
+  end
+
+  def each(z)
+    r = 1 + 0.i
+    4.times do
+      w = z * r
       w = w.imag.i + w.real // b * b
-      w *= rv
+      w *= r.conj
       cost = (z - w).manhattan * k
       yield w, cost
+      r *= 1.i
+    end
+  end
+
+  def type(z)
+    c1 = z.real.divisible_by?(b)
+    c2 = z.imag.divisible_by?(b)
+    case
+    when c1 && c2 then Road::Cross
+    when c1       then Road::Tate
+    when c2       then Road::Yoko
+    else               Road::Town
     end
   end
 end
