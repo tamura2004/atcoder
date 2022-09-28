b4f53eeb3722321947ace92484d6770a8f1b5ca5
M.Tamura <tamura2004@gmail.com>
2022-09-28 23:31:53 +0900

    Add FPS problem

--- src/atcoder/abc207/abc207_f.cr
+++ src/atcoder/abc207/abc207_f.cr
@@ -17,16 +17,19 @@
 # = x (x + 0 + x)^2
 # = 4x^3 -> 3つ警備されていて点0が1であるのは4通り　ok
 
-require "crystal/indexable"
-
-class Polynomial(T)
+class FormalPowerSeries(T)
   getter n : Int32
   getter a : Array(T)
   delegate "[]=", "size", to: a
 
-  def initialize(_a : Array(U)) forall U
-    @a = _a.map { |v| T.new(v) }.reverse
-    @n = _a.size
+  def initialize
+    @n = 0
+    @a = [] of T
+  end
+
+  def initialize(a : Array(U)) forall U
+    @a = a.map { |v| T.new(v) }
+    @n = a.size
   end
 
   def initialize(n : Int)
@@ -35,33 +38,27 @@ class Polynomial(T)
   end
 
   def [](i)
-    if i < n
-      a[i]
-    else
-      T.zero
+    i < n ? a[i] : T.zero
+  end
+
+  def <<(v)
+    @a << T.new(v)
+    @n += 1
+  end
+
+  {% for op in %w(+ -) %}
+    def {{op.id}}(b : self)
+      m = Math.max(n, b.n)
+      FormalPowerSeries(T).new(
+        Array.new(m) do |i|
+          self[i] {{op.id}} b[i]
+        end
+      )
     end
-  end
-
-  def +(b : self)
-    m = Math.max(n, b.n)
-    Polynomial(T).new(
-      Array.new(m) do |i|
-        self[i] + b[i]
-      end
-    )
-  end
-
-  def -(b : self)
-    m = Math.max(n, b.n)
-    Polynomial(T).new(
-      Array.new(m) do |i|
-        self[i] - b[i]
-      end
-    )
-  end
+  {% end %}
 
   def *(b : self)
-    Polynomial(T).new((n - 1) + (b.n - 1) + 1).tap do |ans|
+    FormalPowerSeries(T).new((n - 1) + (b.n - 1) + 1).tap do |ans|
       n.times do |i|
         b.n.times do |j|
           ans[i + j] += a[i] * b[j]
@@ -71,14 +68,14 @@ class Polynomial(T)
   end
 
   def to_s
-    # a.zip(0..).reverse.map{|v,i| "#{v}x^#{i}".gsub(/x\^0/,"").gsub(/x\^1/,"x").gsub(/^1x/,"x")}.join(" + ")
     a.zip(0..).reverse.map do |v, i|
-      if i == 0
-        v.zero? ? nil : "#{v}"
-      elsif i == 1
-        v.zero? ? nil : v == 1 ? "x" : "#{v}x"
-      else
-        v.zero? ? nil : v == 1 ? "x^#{i}" : "#{v}x^#{i}"
+      case
+      when v.zero?          then nil
+      when i.zero?          then "#{v}"
+      when i == 1 && v == 1 then "x"
+      when i == 1           then "#{v}x"
+      when v == 1           then "x^#{i}"
+      else                       "#{v}x^#{i}"
       end
     end.compact.join(" + ")
   end
@@ -88,7 +85,12 @@ class Polynomial(T)
   end
 end
 
+require "crystal/mod_int"
+
+alias FPS = FormalPowerSeries(ModInt)
+
 require "crystal/graph"
+X = FPS{0,1}
 
 n = gets.to_s.to_i
 g = Graph.new(n)
@@ -96,7 +98,29 @@ g = Graph.new(n)
   g.read
 end
 
-f = Polynomial(Int64).new([2, 3])
-g = Polynomial(Int64).new([1, 1])
-pp f + g
-pp f - g
+dp_a = Array.new(n) { FPS.new }
+dp_b = Array.new(n) { FPS.new }
+dp_c = Array.new(n) { FPS.new }
+
+dfs = uninitialized (Int32, Int32) -> Nil
+dfs = -> (v : Int32, pv : Int32) do
+  abxc = FPS{1} # PI nv in ch, (dp_a[nv] + dp_b[nv] + x * dp_c[nv])
+  abc = FPS{1} # PI nv in ch, (dp_a[nv] + dp_b[nv] + dp_c[nv])
+  bc = FPS{1} # PI nv in ch, (dp_b[nv] + dp_c[nv])
+
+  g.each(v) do |nv|
+    next if nv == pv
+    dfs.call(nv, v)
+
+    abxc *= dp_a[nv] + dp_b[nv] + dp_c[nv] * X
+    abc *= dp_a[nv] + dp_b[nv] + dp_c[nv]
+    bc *= dp_b[nv] + dp_c[nv]
+  end
+
+  dp_a[v] = abxc * X
+  dp_b[v] = abc * X - bc * X
+  dp_c[v] = bc
+end
+dfs.call(0, -1)
+
+puts (dp_a[0] + dp_b[0] + dp_c[0]).a.join("\n")
