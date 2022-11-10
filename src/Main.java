2a5a1977a833d0b256d2c732cee4652b78a51748
M.Tamura <tamura2004@gmail.com>
2022-11-10 21:05:51 +0900

    Add java pq sample

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,50 @@
 import java.util.Scanner;
 import java.util.PriorityQueue;
-import java.util.Comparator;
 
 class Main{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-    int m = sc.nextInt();
+		// Scanner sc = new Scanner(System.in);
+		// int n = sc.nextInt();
+    // int m = sc.nextInt();
 
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Comparator.reverseOrder());
+		PriorityQueue<Node> pq = new PriorityQueue<Node>();
 
-		for(int i = 0; i < n; i++) {
-			pq.add(sc.nextInt());
+    pq.add(new Node(1, 40l));
+    pq.add(new Node(2, 30l));
+    pq.add(new Node(3, 20l));
+    pq.add(new Node(4, 50l));
+    pq.add(new Node(5, 60l));
+
+		for(int i = 0; i < 5; i++) {
+		  System.out.println(pq.poll());
     }
-		for(int i = 0; i < m; i++) {
-			pq.add(pq.poll() / 2);
-    }
-		System.out.println(pq.stream().mapToLong(i->i).sum());
-    sc.close();
 	}
+}
+
+class Node implements Comparable<Node> {
+  private Integer id;
+  private Long cost;
+
+  public Node(Integer id, Long cost) {
+    this.id = id;
+    this.cost = cost;
+  }
+
+  public Integer getId() {
+    return this.id;
+  }
+
+  public Long getCost() {
+    return this.cost;
+  }
+
+  @Override
+  public String toString() {
+    return "id = " + id + ", cost = " + cost;
+  }
+
+  @Override
+  public int compareTo(Node other) {
+    return Long.compare(this.cost, other.getCost());
+  }
 }
