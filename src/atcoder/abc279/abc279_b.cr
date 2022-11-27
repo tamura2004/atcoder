75bcd339b974deaa9dc91c4caae138f2e3686a3f
M.Tamura <tamura2004@gmail.com>
2022-11-27 21:58:07 +0900

    Add problem

--- src/atcoder/abc279/abc279_b.cr
+++ src/atcoder/abc279/abc279_b.cr
@@ -1,12 +1,3 @@
 s = gets.to_s
 t = gets.to_s
-
-quit "No" if s.size < t.size
-
-n = s.size
-m = t.size
-n.times do |i|
-  quit "Yes" if s[i,m] == t
-end
-
-puts "No"
+puts s.index(t) ? "Yes" : "No"
--- src/atcoder/abc279/abc279_f.cr
+++ src/atcoder/abc279/abc279_f.cr
@@ -1,53 +1,40 @@
-require "crystal/union_find"
+class HashedUnionFind(T)
+  getter par : Hash(T, T)
 
-n, q = gets.to_s.split.map(&.to_i)
-tot = n
-uf = (n + q + 1).to_uf
-
-# 所属する箱番号、ボールから箱、なければ-1
-box = Array.new(n+q+1, -1)
-(1..n).each do |i|
-  box[i] = i
-end
-
-# 箱に入っている代表番号（ufの親）なければ-1
-#箱からボール
-ball = Array.new(n+1, &.itself)
-
-q.times do
-  cmd, x, y = gets.to_s.split.map(&.to_i) + [0]
-
-  case cmd
-  when 1
-    if ball[x] == -1
-      if ball[y] == -1
-      else
-        box[uf.find(ball[y])] = x
-        ball[x] = uf.find(ball[y])
-        ball[y] = -1
-      end
-    else
-      if ball[y] == -1
-      else
-        uf.unite ball[x], ball[y]
-        box[uf.find(ball[x])] = x
-        ball[x] = uf.find(ball[y])
-        ball[y] = -1
-        # pp! box[uf.find(6)]
-      end
+  def initialize
+    @par = Hash(T, T).new do |h, k|
+      h[k] = k
     end
-  when 2
-    tot += 1
-    if ball[x] == -1
-      ball[x] = tot
-      box[tot] = x
-    else
-      uf.unite ball[x], tot
-      box[uf.find(tot)] = x
-    end
-  when 3
-    puts box[uf.find(x)]
+  end
+
+  def root(v)
+    v == par[v] ? v : (par[v] = root(par[v]))
+  end
+
+  def same?(v, nv)
+    root(v) == root(nv)
+  end
+
+  def unite(v, nv)
+    par[root(nv)] = root(v)
   end
 end
 
+struct Bool
+  def to_s
+    self ? "Yes" : "No"
+  end
+end
 
+n, q = gets.to_s.split.map(&.to_i)
+uf = HashedUnionFind(Int64).new
+
+q.times do
+  cmd, v, nv = gets.to_s.split.map(&.to_i64)
+  case cmd
+  when 0
+    uf.unite v, nv
+  when 1
+    puts uf.same?(v, nv)
+  end
+end
