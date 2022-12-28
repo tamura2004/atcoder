4e05e7870666df1778281236d7d5ba67e5a1c067
M.Tamura <tamura2004@gmail.com>
2022-12-28 20:36:08 +0900

    Add problem

--- src/atcoder/abc282/abc282_e.cr
+++ src/atcoder/abc282/abc282_e.cr
@@ -1,16 +1,25 @@
 require "crystal/union_find"
 require "crystal/number_theory/modpow"
-require "crystal/priority_queue"
 
 n, m = gets.to_s.split.map(&.to_i64)
 a = gets.to_s.split.map(&.to_i64)
+uf = n.to_uf
 
-cnt = PQ(Tuple(Int64,Int32,Int32)).new
-
-(0...n).to_a.each_combination(2) do |(i,j)|
-  cnt << {modpow(a[i],a[j],m),i,j}
+calc = ->(v : Int32, nv : Int32) do
+  (modpow(a[v], a[nv], m) + modpow(a[nv], a[v], m)) % m
 end
 
-while cnt.size > 0
-  pp cnt.pop
+edges = [] of Tuple(Int64, Int32, Int32)
+(0...n).to_a.each_combination(2) do |(i, j)|
+  edges << {calc.call(i, j), i, j}
 end
+edges.sort!
+
+ans = 0_i64
+edges.reverse_each do |cost, v, nv|
+  next if uf.same?(v, nv)
+  uf.unite(v, nv)
+  ans += cost
+end
+
+pp ans
