3193ccfa289fdbfb2faac4a10e39a64c841b3330
M.Tamura <tamura2004@gmail.com>
2023-03-05 21:17:39 +0900

    Add problem

--- src/atcoder/abc292/abc292_h.cr
+++ src/atcoder/abc292/abc292_h.cr
@@ -1,62 +1,30 @@
-require "crystal/lazy_segment_tree"
+require "crystal/segment_tree"
+alias X = Tuple(Int64, Int64)
 
-alias X = Tuple(Int32, Float64)
-alias A = Float64
-
-n, b, q = gets.to_s.split.map(&.to_i)
-
-fxx = Proc(X, X, X).new do |(i, x), (j, y)|
-  pp! [i,x,j,y]
-  x,y = y,x unless i < j
-  i,j = j,i unless i < j
-  z = b <= x ? x : Math.max(x,y)
-  X.new(Math.min(i, j), z)
-end
-
-fxa = Proc(X, A, X).new { |(i, x), a| X.new(i, x + a / i) }
-faa = Proc(A, A, A).new { |a, b| a + b }
-
-values = Array.new(n) do |i|
-  X.new(i+1, 0.0_f64)
-end
-
-# 区間平均加算、区間最大
-st = LazySegmentTree(X, A).new(
+n, b, q = gets.to_s.split.map(&.to_i64)
+values = gets.to_s.split.map(&.to_i64.-(b)).map { |v| {v, v} }
+st = SegmentTree(X).new(
   values,
-  fxx,
-  fxa,
-  faa,
-  X.new(Int32::MAX, 0.0_f64),
-  A.zero
+  unit: X.new(0_i64, Int64::MIN//4),
+  fx: -> (x : X, y : X) {
+    X.new(x[0] + y[0], Math.max(x[1], x[0] + y[1]))
+  }
 )
 
-a = gets.to_s.split.map(&.to_i64)
-sum = a.sum
-
-n.times do |i|
-  st[i..] = a[i].to_f64
-end
-
-q.times do
+q.times do |k|
   i, x = gets.to_s.split.map(&.to_i64)
   i -= 1
+  st[i] = ({x - b, x - b})
+ 
+  j = (0...n).bsearch do |i|
+    st[..i][1] >= 0
+  end
 
-  st[i...] = (x - a[i]).to_f64
-
-  sum -= a[i]
-  sum += x
-  a[i] = x
-
-  if b <= st[0..][1]
-    puts st[0..][1]
+  if j.nil?
+    tot = st[0..][0]
+    puts (tot + b * n) / n
   else
-    puts sum / n
+    tot = st[..j][0]
+    puts (tot + b * (j + 1)) / (j + 1)
   end
-
-  5.times do |i|
-    pp st[i]
-  end
-  pp st[0..]
-  pp st.fold(0,n)
 end
-
