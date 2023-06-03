a05c1389fec4188a2a9153013d108e5c59680ca6
M.Tamura <tamura2004@gmail.com>
2023-06-03 10:31:46 +0900

    Add lib

--- src/play.cr
+++ src/play.cr
@@ -1,7 +1,25 @@
-require "big"
+# 作用素
+# x <- Xに対して、作用（apply）
+# chmax x, mini
+# chmin x, maxi
+# x += incr
+# かつ、合成(compose)ができる
+class A
+  getter mini : Int32
+  getter maxi : Int32
+  getter incr : Int32
 
-t = gets.to_s.to_i64
-t.times do
-  a, b = gets.to_s.split.map(&.to_big_i)
-  pp a + b
+  def initialize(@mini, @maxi, @incr)
+  end
+
+  def compose(b : self)
+    
+  end
+
+  def apply(x)
+    chmax x, mini
+    chmin x, maxi
+    x + incr
+  end
 end
+
