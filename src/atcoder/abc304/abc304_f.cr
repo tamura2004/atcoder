574f76e0bded99c10826c7a0e262e9c7f6f925c7
M.Tamura <tamura2004@gmail.com>
2023-06-08 18:01:06 +0900

    Update problem

--- src/atcoder/abc304/abc304_f.cr
+++ src/atcoder/abc304/abc304_f.cr
@@ -1,35 +1,73 @@
 require "crystal/modint9"
-require "crystal/prime"
+
+MAX = 500_000i64
+
+class Seive
+  getter min_factor : Array(Int64)
+  getter moebius : Array(Int64)
+  getter is_prime : Array(Bool)
+
+  def initialize
+    @min_factor = Array.new(MAX+1, &.itself.to_i64)
+    @moebius = Array.new(MAX+1, 1_i64)
+    @is_prime = Array.new(MAX+1, true)
+
+    min_factor[1] = 1
+    moebius[1] = 1
+    is_prime[1] = false
+
+    (2_i64..MAX).each do |i|
+      next unless is_prime[i]
+      moebius[i] = -1_i64
+      (i * 2).step(by: i, to: MAX) do |j|
+        is_prime[j] = false
+        min_factor[j] = i
+        moebius[j] *= (j // i).divisible_by?(i) ? 0_i64 : -1_i64
+      end
+    end
+  end
+end
+
+def factors(n)
+  m = 1_i64
+  ans = [] of Int64
+  while m * m <= n
+    if n.divisible_by?(m)
+      ans << m
+      ans << n // m unless m * m == n
+    end
+    m += 1
+  end
+  ans
+end
+
+sv = Seive.new
 
 n = gets.to_s.to_i64
 s = gets.to_s.chars.map(&.==('#').to_unsafe)
 
-ft = n.prime_factors
-
-solve = -> (e : Int64) do
-  m = n // e
-  cnt = Array.new(m, 1)
-  n.times do |i|
-    cnt[i % m] &= s[i]
+query = -> (m : Int64) do
+  cnt = Array.new(m, 1_i64)
+  s.each_with_index do |v, i|
+    cnt[i % m] *= v
   end
   2.to_m ** cnt.sum
 end
 
-ans = 0.to_m
-(1<<(ft.size)).times do |mask|
-  next if mask.zero?
-
-  acc = 1_i64
-  ft.each_with_index do |v, i|
-    next if mask.bit(i).zero?
-    acc = acc.lcm(v)
-  end
-
-  if mask.popcount.odd?
-    ans += solve.call(acc)
-  else
-    ans -= solve.call(acc)
-  end
+a = Hash(Int64,ModInt).new
+factors(n).each do |d|
+  next if n == d
+  a[d] = query.call(d)
 end
 
-pp ans
+b = Hash(Int64,ModInt).new
+factors(n).each do |m|
+  next if n == m
+  ans = 0.to_m
+  factors(m).each do |d|
+    ans += a[d] * sv.moebius[m//d]
+  end
+  b[m] = ans
+end
+
+puts b.values.sum
--- src/play3.cr
+++ src/play3.cr
@@ -1,18 +1,41 @@
-require "crystal/prime"
+class Sieve
+  # MAX = 300_000_i64
+  MAX = 30_i64
+  getter is_prime : Array(Bool)
+  getter min_factor : Array(Int64)
+  getter moebius : Array(Int64)
 
-def each(acc, n, &block : Array(Int32) -> Nil)
-  if acc.size == n
-    block.call acc
-  else
-    if acc[-1] == 0
-      each(acc + [0], n, &block)
-      each(acc + [1], n, &block)
-    else
-      each(acc + [0], n, &block)
+  def initialize
+    @is_prime = Array.new(MAX + 1, true)
+    @min_factor = Array.new(MAX + 1, 0_i64)
+    @moebius = Array.new(MAX + 1, 1_i64)
+
+    is_prime[0] = false
+    is_prime[1] = false
+    min_factor[0] = 0_i64
+    min_factor[1] = 1_i64
+    moebius[0] = 0_i64
+    moebius[1] = 1_i64
+
+    (2i64..MAX).each do |i|
+      next unless is_prime[i]
+      min_factor[i] = i
+      moebius[i] = -1_i64
+
+      (i*2).step(by: i, to: MAX) do |j|
+        is_prime[j] = false
+        min_factor[j] = i if min_factor[j] == 0
+        if (j // i).divisible_by?(i)
+          moebius[j] = 0_i64
+        else
+          moebius[j] *= -1
+        end
+      end
     end
   end
 end
 
-each([0], 8) do |arr|
-  pp arr
-end
+sv = Sieve.new
+pp sv.is_prime.zip(0..)
+pp sv.min_factor.zip(0..)
+pp sv.moebius.zip(0..)
