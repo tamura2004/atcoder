e96a3fcdd70b44fb18c82e18d595a08e9c1c785e
M.Tamura <tamura2004@gmail.com>
2023-07-24 19:09:02 +0900

    Add problem

--- src/main.rb
+++ src/main.rb
@@ -1,32 +1,67 @@
-<<<<<<< HEAD
-def n(s)
-x,y=s.min
-s.map{|i,j|[i-x,j-y]}.to_set
-end
-a,b,c=(0...3).map{h,w=gets.to_s.split.map &.to_i
-n((0...h).map{|y|s=gets.to_s
-(0...w).map{|x|s[x]=='#'?[y,x]: nil
-}}.flatten.compact.to_set)}r=(-20..20)
-r.each{|x|r.each{|y|puts(:Yes)+exit if n(a.map{|i,j|[i+x,j+y]}.to_set+b)==c}}
-puts :No
-=======
-n, m = gets.split.map(&:to_i)
+class Grid
+  attr_accessor :h
+  attr_accessor :w
+  attr_accessor :s
+  attr_accessor :g
 
-# mod[i][j] := jがi個並んだ数字をmで割った余り
-mod = Array.new(n + 1) { Array.new(11, 0) }
-11.times do |j|
-  (1..n).each do |i|
-    mod[i][j] = (mod[i - 1][j] * 10 + j) % m
-  end
-end
-
-(1..n).reverse_each do |i|
-  (1..9).reverse_each do |j|
-    if mod[i][j] == 0
-      puts j.to_s * i
-      exit
+  def initialize(h, w, s)
+    @h = h
+    @w = w
+    @s = s
+    @g = Array.new(h) { Array.new(w, 0) }
+    h.times do |y|
+      w.times do |x|
+        g[y][x] = s[y * w + x]
+      end
     end
   end
+
+  def [](y, x)
+    if 0 <= x && x < w && 0 <= y && y < h
+      g[y][x]
+    else
+      0
+    end
+  end
+
+  def white?(y, x)
+    self[y, x] == 0
+  end
+
+  def black?(y, x)
+    !white?(y, x)
+  end
+
+  def valid?
+    h.times.all? do |y|
+      w.times.all? do |x|
+        white?(y - 1, x) && white?(y - 1, x - 1) || black?(y, x)
+      end
+    end
+  end
+
+  def c(i)
+    s[i] == 1 ? "●" : "○"
+  end
+
+  def to_s
+    ans = g.map do |row|
+      row.map do |cell|
+        cell == 1 ? "●" : "○"
+      end.join(" ")
+    end.join("\n")
+
+    "===\n#{s}\n" + ans
+  end
 end
-puts -1
->>>>>>> a60674153b1209a7ebb072393d7a99c64df8b0a8
+
+cnt = 0
+(1 << 12).times do |s|
+  g = Grid.new(3, 3, s)
+  next unless g.valid?
+  puts g
+  cnt += 1
+end
+
+pp "---"
+pp cnt
