f971b21a72dc1041a7776fac29d7dbc6432e3f12
M.Tamura <tamura2004@gmail.com>
2023-08-25 12:30:56 +0900

    Update lib

--- src/main.swift
+++ src/main.swift
@@ -1,16 +1,46 @@
 import Foundation
 
-let hiragana: [Character] = {
-  [UnicodeScaler("あ").value...UnicodeScalar("ん").value].joined()
-    .compactMap { value in
-      UnicodeScalar(value).map(Character.init)
-    }
-}()
+enum Muki {
+  case Yoko
+  case Tate
 
-struct PC {
-  var name: String = "名無しさん"
-
-  init() {
-
+  var gyaku: Muki {
+    return self == .Yoko ? .Tate : .Yoko
   }
 }
+
+struct MukiArray<T> {
+  var a: [[T]]
+
+  init(from: [[T]]) {
+    self.a = from
+  }
+
+  subscript(d: Muki, i:Int, j:Int) -> T {
+    get {
+      switch d {
+        case .Tate:
+          return a[i][j]
+        case .Yoko:
+          return a[j][i]
+      }
+    }
+
+    set(value) {
+      switch d {
+        case .Tate:
+          self.a[i][j] = value
+        case .Yoko:
+          self.a[j][i] = value
+      }
+    }
+  }
+}
+
+var dic: [Muki: Int] = [.Yoko: 10, .Tate: 20]
+var mukiArray = MukiArray(from: [[1,2,3],[4,5,6]])
+
+print(dic[.Yoko.gyaku]!)
+print(dic[.Tate.gyaku]!)
+print(mukiArray[.Yoko, 0, 1])
+// print(mukiArray[.Tate, 0, 1])
