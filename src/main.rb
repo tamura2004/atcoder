1b687b337842bc0194816d09701d7cf466fcce6d
M.Tamura <tamura2004@gmail.com>
2023-09-06 17:52:42 +0900

    Add scala segment tree

--- src/main.rb
+++ src/main.rb
@@ -1,81 +1,79 @@
-hiragana = [
-  "あ",
-  "い",
-  "う",
-  "え",
-  "お",
-  "か",
-  "が",
-  "き",
-  "ぎ",
-  "く",
-  "ぐ",
-  "け",
-  "げ",
-  "こ",
-  "ご",
-  "さ",
-  "ざ",
-  "し",
-  "じ",
-  "す",
-  "ず",
-  "せ",
-  "ぜ",
-  "そ",
-  "ぞ",
-  "た",
-  "だ",
-  "ち",
-  "ぢ",
-  "つ",
-  "づ",
-  "て",
-  "で",
-  "と",
-  "ど",
-  "な",
-  "に",
-  "ぬ",
-  "ね",
-  "の",
-  "は",
-  "ば",
-  "ぱ",
-  "ひ",
-  "び",
-  "ぴ",
-  "ふ",
-  "ぶ",
-  "ぷ",
-  "へ",
-  "べ",
-  "ぺ",
-  "ほ",
-  "ぼ",
-  "ぽ",
-  "ま",
-  "み",
-  "む",
-  "め",
-  "も",
-  "や",
-  "ゆ",
-  "よ",
-  "ら",
-  "り",
-  "る",
-  "れ",
-  "ろ",
-  "わ",
+names = [
+  {
+    name: "もねた",
+    branches: [
+      "ふたご",
+      "れちくる",
+      "てんびん",
+    ],
+  },
+  {
+    name: "かりす",
+    branches: [
+      "おひつじ",
+      "おうし",
+      "かに",
+    ],
+  },
+  {
+    name: "きるけ",
+    branches: [
+      "しし",
+      "おとめ",
+      "さそり",
+    ],
+  },
+  {
+    name: "てみす",
+    branches: [
+      "みずがめ",
+      "うお",
+      "みなみのさんかく",
+    ],
+  },
+  {
+    name: "ぽぽす",
+    branches: [
+      "いて",
+      "やぎ",
+      "らしんばん",
+    ],
+  },
+  {
+    name: "もねたカード",
+    branches: [
+      "もねたカード",
+    ],
+  },
 ]
 
-10.times do
-  name = hiragana.sample(2)
-  if rand < 0.5
-    name.unshift name[0]
-  else
-    name << name[-1]
+bankd = {}
+branch = {}
+branch_id = 0
+
+names.each_with_index do |bank, id|
+  bankd["100#{id}"] = {
+    "bango" => "100#{id}",
+    "kanji_name" => bank[:name] + (bank[:name] =~ /カード/ ? "" : "銀行"),
+    "kana_name" => bank[:name],
+  }
+
+  bank[:branches].each_with_index do |name, i|
+    branch_id += 1
+    key = (100 + branch_id).to_s
+    branch[key] = {
+      "bank_bango" => "100#{id}",
+      "shiten_bango" => key,
+      "kanji_name" => name + (name =~ /カード/ ? "" : "座支店"),
+      "nama_name" => name,
+    }
   end
-  puts name.join
+end
+
+open("bank.rb", "w") do |fh|
+  fh.puts bankd.inspect
+end
+
+open("branch.rb", "w") do |fh|
+  fh.puts branch.inspect
 end
