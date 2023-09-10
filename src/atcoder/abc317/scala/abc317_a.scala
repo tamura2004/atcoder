a002c66fa76e34d0d89346fb303fd552ceb0e1d3
mitsuhiro tamura <tamura2004@gmail.com>
2023-09-10 11:54:55 +0900

    Add abc317 java

---
+++ src/atcoder/abc317/scala/abc317_a.scala
@@ -0,0 +1,15 @@
+// import java.util.Scanner
+
+// object Main extends App {
+//   val sc = new Scanner(System.in)
+//   val n = sc.nextInt
+
+
+
+import java.util.Scanner
+
+object Main extends App {
+  val sc = new Scanner(System.in)
+  val n = sc.nextInt
+  printkln(n)
+}
