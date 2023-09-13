aabea9844da4a0849c3f5105a8ccf0a864aadebb
M.Tamura <tamura2004@gmail.com>
2023-09-14 00:58:38 +0900

    Add problem

--- src/main.rb
+++ src/main.rb
@@ -1,2 +1,14 @@
-pp `dd`.gsub /\n/, ""
-pp `dd`.lines
+x, y, z = gets.split.map(&:to_i)
+if x < 0
+  x *= -1
+  y *= -1
+  z *= -1
+end
+
+if y < 0 || x < y || (z < y && 0 < z)
+  pp x
+elsif z < 0
+  pp z.abs + (x - z).abs
+else
+  p -1
+end
