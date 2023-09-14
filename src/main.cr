976a89ce728b8bbf5d0f3c9d96fc90de9b1b34d7
M.Tamura <tamura2004@gmail.com>
2023-09-14 23:19:02 +0900

    Update problem

--- src/main.cr
+++ src/main.cr
@@ -1,2 +1,7 @@
-a,b,c=gets.to_s.split.map(&.to_i)
-puts b.zero? && a == c ? "?" : a + b == c ? "+" : a - b == c ? "-" : "!"
+n,a,b=gets.to_s.split.map(&.to_i)
+ans = (1..n).select do |i|
+  x = i.digits.sum
+  (x - a) * (x - b) <= 0
+end.sum
+
+pp ans
