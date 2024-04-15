e4b7a86adb75dd1156d41a0847828bd4ae2dcd3b
tamura2004 <tamura2004@gmail.com>
2024-04-15 14:28:27 +0000

    Add problem

--- src/atcoder/abc296/abc296_e.cr
+++ src/atcoder/abc296/abc296_e.cr
@@ -1,37 +1,13 @@
+require "crystal/graph/tsort"
 require "crystal/graph"
 
-n = gets.to_s.to_i
-a = gets.to_s.split.map(&.to_i64)
+n = gets.to_s.to_i64
+a = gets.to_s.split.map(&.to_i.pred)
 g = n.to_g
 
-(1..n).each do |v|
-  g.add v, a[v.pred], both: false
+a.zip(0..).each do |nv, v|
+  g.add v, nv, both: false, origin: 0
 end
 
-indeg = Array.new(n, 0_i64)
-n.times do |v|
-  g.each(v) do |nv|
-    indeg[nv] += 1
-  end
-end
-
-q = Deque(Int32).new
-n.times do |v|
-  if indeg[v] == 0
-    q << v
-  end
-end
-
-ans = n
-while q.size > 0
-  v = q.shift
-  ans -= 1
-  g.each(v) do |nv|
-    indeg[nv] -= 1
-    if indeg[nv] == 0
-      q << nv
-    end
-  end
-end
-
-pp ans
+cnt = Tsort.new(g).solve
+pp n - cnt.size
