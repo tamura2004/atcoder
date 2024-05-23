51c671eaf6a67814be8bb9b797f97a086b2faa75
tamura2004 <tamura2004@gmail.com>
2024-05-23 14:13:34 +0000

    Update lib

--- lib/crystal/liner_function.cr
+++ lib/crystal/liner_function.cr
@@ -1,20 +1,31 @@
+require "crystal/modint9"
+
 # f(x) = a*x + b
 # f + g = g(f(x))
 # x * f = f(x) 
-struct LinerFunction(T)
-  getter a : T
-  getter b : T
-
-  def initialize(@a, @b)
+record F, a : ModInt, b : ModInt do
+  def +(other : F) : F
+    F.new(a * other.a, other.a * b + other.b)
   end
 
-  def +(other : self)
-    self.class.new(a * other.a, other.a * b + other.b)
+  def self.zero
+    F.new(1.to_m, 0.to_m)
   end
 
-  def *(other)
-    other * a + b
+  def **(n : Int32) : F
+    base = self
+    ans = F.zero
+    while n > 0
+      if n.odd?
+        ans += base
+      end
+      n //= 2
+      base += base
+    end
+    ans
+  end
+
+  def [](x)
+    a * x + b
   end
 end
-
-alias F = LinerFunction
--- lib/crystal/liner_function_spec.cr
+++ lib/crystal/liner_function_spec.cr
@@ -1,7 +1,11 @@
 require "spec"
 require "crystal//liner_function"
 
-describe LinerFunction do
+describe "LinerFunction" do
   it "usage" do
+    f = F.new(2.to_m, 1.to_m)
+    (f ** 5).should eq (f + f + f + f + f)
+    (f ** 7).should eq (f + f + f + f + f + f + f)
+    f[5].should eq 11
   end
 end
